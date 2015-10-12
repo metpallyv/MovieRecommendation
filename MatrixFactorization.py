6f7628d3adb1dc3692d74055d2c9fb9ec7a1567f
Vardhaman Metpally <vardhaman.metpally@gmail.com>
2015-10-12 11:33:28 -0700

    Update MatrixFactorization.py

--- MatrixFactorization.py
+++ MatrixFactorization.py
@@ -98,7 +98,7 @@ def main(X,K):
     #steps : the maximum number of steps to perform the optimisation, hardcoding the values
     #alpha : the learning rate, hardcoding the values
     #beta  : the regularization parameter, hardcoding the values
-    steps = 1000
+    steps = 5000
     alpha = 0.0002
     beta = float(0.02)
     estimated_P, estimated_Q = matrix_factorization(X,P,Q,K,steps,alpha,beta)
